b18801f681a90d3f754519af0ba21d25d97f520b
Chris Reynolds <christopher.reynolds@accenture.com>
2016-01-07 16:03:28 +0000

    Adding arrays and hashes example

---
+++ hash.pl
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+# Use local scope (my)
+my @array = ('Orange', 'Apple', 'Banana', 'Cherry');
+my %hash = ('foo' => 'bar', 'baz' => 'qux');
+
+print "Array: \n\n", Dumper (\@array), "\nHash (associative array):\n\n", Dumper (\%hash);
