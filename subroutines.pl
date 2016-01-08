3c6fc549bfc5226fac9879bd9fb9be789ba55564
Chris Reynolds <christopher.reynolds@accenture.com>
2016-01-08 14:53:53 +0000

    Added hyphenate and csv subs

--- subroutines.pl
+++ subroutines.pl
@@ -1,4 +1,19 @@
 use strict;
 use warnings;
 
+# User-defined subroutines always accept the same input: a list of scalars
+sub hyphenate {
+    # Extract the first element from the array
+    my $word = shift @_;
 
+    $word = join '-', map { substr $word, $_, 1 } (0 .. (length $word) - 1);
+
+    return $word;
+}
+
+sub csv {
+
+    return join(', ', @_);
+}
+
+print "\n\nHyphenate sub: ", hyphenate('foo'), "\n\nCSV sub: ", csv(('foo', 'bar', 'baz', 'qux'));
