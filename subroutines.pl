3a84c3ae0a37b359daf9027bfc545387dc7091dd
Chris Reynolds <christopher.reynolds@accenture.com>
2016-01-08 15:38:56 +0000

    Added extracting subroutine variables examples and refactoring

--- subroutines.pl
+++ subroutines.pl
@@ -13,7 +13,36 @@ sub hyphenate {
 
 sub csv {
 
-    return join(', ', @_);
+    return join(', ', shift @_);
 }
 
 print "\n\nHyphenate sub: ", hyphenate('foo'), "\n\nCSV sub: ", csv(('foo', 'bar', 'baz', 'qux'));
+
+# Arguments should be unpacked as Perl calls by reference (original variable values will be overwritten!)
+# First method using $_ and numerical keys
+sub a_function {
+    my $first_var  = $_[0];
+    my $second_var = $_[1];
+    my $third_var  = $_[2];
+}
+
+# Second method using shift to extract arguments one by one
+# Using these methods can make sub scoped variables hard to keep track of 
+sub another_function {
+    my $first_var  = shift @_;
+    my $second_var = shift; # By default shift in this context will be passed @_ (the arguments passed to the sub)
+}
+
+# Third method to assign multiple sub scope variable at once
+sub a_different_function {
+    my ($first_var, $second_var) = @_;
+} 
+
+# Fourth method for accepting a large number of sub arguments via a hash
+sub another_different_function {
+    my %args = @_;
+    my $first_var  = $args{'firstVar'};
+    my $second_var = $args{'secondVar'};
+}
+
+another_different_function({ 'firstVar' => 'foo', 'secondVar' => 'bar' });
